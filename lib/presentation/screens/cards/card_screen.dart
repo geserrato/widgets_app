caebbd30c62b8cb42ff8546364459fe8d2f090d6
Gerardo Estrella <g.serrato41@outlook.com>
2023-05-26 16:02:01 -0600

    se agrega diseños de cards

--- lib/presentation/screens/cards/card_screen.dart
+++ lib/presentation/screens/cards/card_screen.dart
@@ -12,6 +12,7 @@ const cards = <Map<String, dynamic>>[
 
 class CardScreen extends StatelessWidget {
   static const String name = 'cards_screen';
+
   const CardScreen({super.key});
 
   @override
@@ -30,22 +31,42 @@ class _CardView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          ...cards.map(
-            (card) => _CardType1(
-              elevation: card['elevation'],
-              label: card['label'],
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            ...cards.map(
+              (card) => _CardType1(
+                elevation: card['elevation'],
+                label: card['label'],
+              ),
             ),
-          ),
-          ...cards.map(
-            (card) => _CardType1(
-              elevation: card['elevation'],
-              label: card['label'],
+            ...cards.map(
+              (card) => _CardType2(
+                elevation: card['elevation'],
+                label: card['label'],
+              ),
             ),
-          ),
-        ],
+            ...cards.map(
+              (card) => _CardType3(
+                elevation: card['elevation'],
+                label: card['label'],
+              ),
+            ),
+            ...cards.map(
+              (card) => _CardType4(
+                elevation: card['elevation'],
+                label: card['label'],
+              ),
+            ),
+            ...cards.map(
+              (card) => _CardType5(
+                elevation: card['elevation'],
+                label: card['label'],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
@@ -81,6 +102,166 @@ class _CardType1 extends StatelessWidget {
             )
           ],
         ),
+      ),
+    );
+  }
+}
+
+class _CardType2 extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  const _CardType2({
+    required this.label,
+    required this.elevation,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: const BorderRadius.all(
+          Radius.circular(12),
+        ),
+        side: BorderSide(
+          color: colors.outline,
+        ),
+      ),
+      elevation: elevation,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.more_vert_outlined),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Text('$label - outline'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CardType3 extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  const _CardType3({
+    required this.label,
+    required this.elevation,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return Card(
+      color: colors.surfaceVariant,
+      elevation: elevation,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.more_vert_outlined),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Text('$label - Filled'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CardType4 extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  const _CardType4({
+    required this.label,
+    required this.elevation,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return Card(
+      color: colors.surfaceVariant,
+      elevation: elevation,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.more_vert_outlined),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Text('$label - Filled'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CardType5 extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  const _CardType5({
+    required this.label,
+    required this.elevation,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      clipBehavior: Clip.hardEdge,
+      elevation: elevation,
+      child: Stack(
+        children: [
+          Image.network(
+            'https://picsum.photos/id/${elevation.toInt()}/600/250',
+            height: 350,
+            fit: BoxFit.cover,
+          ),
+          Align(
+            alignment: Alignment.topRight,
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(20),
+                ),
+              ),
+              child: IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.more_vert_outlined),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
